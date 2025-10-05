e7212a2a56a35fc27567b5c95823998c802ec9c2
catniss7 <alyssa.la1777@gmail.com>
2025-10-05 16:48:43 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from my_library import app as library_app
 
+app = FastAPI(title="NASA Research Explorer")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/library", library_app)
